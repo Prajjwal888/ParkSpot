9ee6cae6a53036ddb3515f5fdbc9efb2d687d204
Prajjwal888 <chouhanprajjwal8@gmail.com>
2025-01-24 01:07:19 +0530

    update validateForms

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
     'use strict'
     const forms = document.querySelectorAll('.validated-form')
@@ -14,21 +13,4 @@
                 form.classList.add('was-validated')
             }, false)
         })
-=======
-(function () {
-    'use strict'
-    const forms = document.querySelectorAll('.validated-form')
-
-    Array.from(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
-            }, false)
-        })
->>>>>>> 3b96ba7 (add booking functionality)
 })()
